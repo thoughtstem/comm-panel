253f9810d379b6e45a18024dc339f837ad3166df
Stephen <stephen@thoughtstem.com>
2018-06-15 11:43:51 -0700

    providing more

--- main.rkt
+++ main.rkt
@@ -1,5 +1,12 @@
 (module comm-panel racket
   (provide (all-from-out "ui.rkt")
+           (all-from-out "computer-queues.rkt")
+           (all-from-out "string-sending.rkt")
+           (all-from-out "broadcast-listen.rkt")
+
            #%module-begin)
 
-  (require "ui.rkt"))
+  (require "ui.rkt")
+  (require "computer-queues.rkt")
+  (require "string-sending.rkt")
+  (require "broadcast-listen.rkt"))
